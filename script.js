d4e7e9f687c937a3e4081c14637b86aecfd8af65
Anurag Dubey <71247280+Anuragd275@users.noreply.github.com>
2022-06-21 18:57:24 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const elements = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+
+document.getElementById("btn").onclick = function () {
+    document.getElementById('beforetext').innerHTML = "Your new Password is : "
+    document.getElementById("pass").innerHTML = 
+        String( elements[((Math.floor(Math.random() * elements.length)))] +
+         elements[((Math.floor(Math.random() * elements.length)))] +
+         elements[((Math.floor(Math.random() * elements.length)))] +
+         elements[((Math.floor(Math.random() * elements.length)))] +
+         elements[((Math.floor(Math.random() * elements.length)))] +
+         elements[((Math.floor(Math.random() * elements.length)))] +
+         elements[((Math.floor(Math.random() * elements.length)))] +
+         elements[((Math.floor(Math.random() * elements.length)))] +
+         elements[((Math.floor(Math.random() * elements.length)))] +
+         elements[((Math.floor(Math.random() * elements.length)))] +
+         elements[((Math.floor(Math.random() * elements.length)))] +
+         elements[((Math.floor(Math.random() * elements.length)))] )
+    
+ }
+
+
