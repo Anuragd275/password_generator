c32c6d9078609b0707b7a248783ff7c1d0488dd2
Anurag Dubey <71247280+Anuragd275@users.noreply.github.com>
2022-06-25 17:45:34 +0530

    Updated Script

--- script.js
+++ script.js
@@ -1,26 +1,77 @@
-const elements = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+const resultEl = document.getElementById('result')
+const lengthEl = document.getElementById('length')
+const uppercaseEl = document.getElementById('uppercase')
+const lowercaseEl = document.getElementById('lowercase')
+const numbersEl = document.getElementById('numbers')
+const symbolsEl = document.getElementById('symbols')
+const generateEl = document.getElementById('generate')
+const clipboardEl = document.getElementById('clipboard')
 
-document.getElementById("btn").onclick = function () {
-    document.getElementById('beforetext').innerHTML = "Your new Password is : "
-    document.getElementById("pass").innerHTML = 
-        String( elements[((Math.floor(Math.random() * elements.length)))] +
-         elements[((Math.floor(Math.random() * elements.length)))] +
-         elements[((Math.floor(Math.random() * elements.length)))] +
-         elements[((Math.floor(Math.random() * elements.length)))] +
-         elements[((Math.floor(Math.random() * elements.length)))] +
-         elements[((Math.floor(Math.random() * elements.length)))] +
-         elements[((Math.floor(Math.random() * elements.length)))] +
-         elements[((Math.floor(Math.random() * elements.length)))] +
-         elements[((Math.floor(Math.random() * elements.length)))] +
-         elements[((Math.floor(Math.random() * elements.length)))] +
-         elements[((Math.floor(Math.random() * elements.length)))] +
-         elements[((Math.floor(Math.random() * elements.length)))] )
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+}
+
+clipboardEl.addEventListener('click', () => {
+    const textarea = document.createElement('textarea')
+    const password = resultEl.innerText
+
+    if(!password) { return }
+
+    textarea.value = password
+    document.body.appendChild(textarea)
+    textarea.select()
+    document.execCommand('copy')
+    textarea.remove()
+    alert('Password copied to clipboard!')
+})
+
+generateEl.addEventListener('click', () => {
+    const length = +lengthEl.value
+    const hasLower = lowercaseEl.checked
+    const hasUpper = uppercaseEl.checked
+    const hasNumber = numbersEl.checked
+    const hasSymbol = symbolsEl.checked
+
+    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length)
+})
+
+function generatePassword(lower, upper, number, symbol, length) {
+    let generatedPassword = ''
+    const typesCount = lower + upper + number + symbol
+    const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0])
     
- }
+    if(typesCount === 0) {
+        return ''
+    }
 
+    for(let i = 0; i < length; i += typesCount) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0]
+            generatedPassword += randomFunc[funcName]()
+        })
+    }
 
- let c = document.getElementById('c')
+    const finalPassword = generatedPassword.slice(0, length)
 
-if(c){
-    alert("AaA hu")
+    return finalPassword
+}
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+}
+
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+}
+
+function getRandomSymbol() {
+    const symbols = '!@#$%^&*(){}[]=<>/,.'
+    return symbols[Math.floor(Math.random() * symbols.length)]
 }
